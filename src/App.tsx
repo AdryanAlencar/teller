b0e4d7b051e1aa8891c8e870632ca915db36ce34
Adryan Alencar <adryan.alencar@sevenfox.com.br>
2022-01-17 02:54:02 -0300

    Correçao de bugs de produçao

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,305 @@ import React from 'react';
 import { Message } from './components/message';
 import styles from './styles.module.scss';
 
-import Messages from './messages';
-
 function App() {
-  var messages = Messages();
+  var messages = {
+    "amor": [
+        {
+            "phrase": "Pois estou convencido de que nem morte nem vida, nem anjos nem demônios, nem o presente nem o futuro, nem quaisquer poderes, nem altura nem profundidade, nem qualquer outra coisa na criação será capaz de nos separar do amor de Deus que está em Cristo Jesus, nosso Senhor.",
+            "reference": "Romanos 8:38-39"
+        },
+        {
+            "phrase": " 'Porque Deus tanto amou o mundo que deu o seu Filho Unigênito, para que todo o que nele crer não pereça, mas tenha a vida eterna.",
+            "reference": "João 3:16"
+        },
+        {
+            "phrase": "Deem graças ao Senhor, porque ele é bom. O seu amor dura para sempre!",
+            "reference": "Salmos 136:1"
+        },
+        {
+            "phrase": "Amados, não escrevo a vocês um mandamento novo, mas um mandamento antigo, que vocês têm desde o princípio: a mensagem que ouviram. No entanto, o que escrevo é um mandamento novo, o qual é verdadeiro nele e em vocês, pois as trevas estão se dissipando e já brilha a verdadeira luz.",
+            "reference": "1 João 2:7-8"
+        },
+        {
+            "phrase": "Todavia, Deus, que é rico em misericórdia, pelo grande amor com que nos amou, deu-nos vida com Cristo quando ainda estávamos mortos em transgressões - pela graça vocês são salvos.",
+            "reference": "Efésios 2:4-5"
+        },
+        {
+            "phrase": "Porque nisto consiste o amor a Deus: em obedecer aos seus mandamentos. E os seus mandamentos não são pesados.",
+            "reference": "1 João 5:3"
+        },
+        {
+            "phrase": "Mas Deus demonstra seu amor por nós: Cristo morreu em nosso favor quando ainda éramos pecadores.",
+            "reference": "Romanos 5:8"
+        },
+        {
+            "phrase": "Fui crucificado com Cristo. Assim, já não sou eu quem vive, mas Cristo vive em mim. A vida que agora vivo no corpo, vivo-a pela fé no filho de Deus, que me amou e se entregou por mim.",
+            "reference": "Gálatas 2:20"
+        },
+        {
+            "phrase": "Foi assim que Deus manifestou o seu amor entre nós: enviou o seu Filho Unigênito ao mundo, para que pudéssemos viver por meio dele. Nisto consiste o amor: não em que nós tenhamos amado a Deus, mas em que ele nos amou e enviou seu Filho como propiciação pelos nossos pecados. Amados, visto que Deus assim nos amou, nós também devemos amar uns aos outros.",
+            "reference": "1 João 4:9-11"
+        },
+        {
+            "phrase": "O Senhor, o seu Deus, está em seu meio, poderoso para salvar. Ele se regozijará em você; com o seu amor a renovará, ele se regozijará em você com brados de alegria",
+            "reference": "Sofonias 3:17"
+        },
+        {
+            "phrase": "Mas tu, Senhor, és Deus compassivo e misericordioso, muito paciente, rico em amor e em fidelidade.",
+            "reference": "Salmos 86:15"
+        },
+        {
+            "phrase": "Vejam como é grande o amor que o Pai nos concedeu: sermos chamados filhos de Deus, o que de fato somos! Por isso o mundo não nos conhece, porque não o conheceu.",
+            "reference": "1 João 3:1"
+        },
+        {
+            "phrase": "Amo os que me amam, e quem me procura me encontra.",
+            "reference": "Provérbios 8:17"
+        },
+        {
+            "phrase": "O Senhor é compassivo e misericordioso, mui paciente e cheio de amor.",
+            "reference": "Salmos 103:8"
+        },
+        {
+            "phrase": "o Senhor lhe apareceu no passado, dizendo:  'Eu a amei com amor eterno; com amor leal a atraí.",
+            "reference": "Jeremias 31:3"
+        },
+        {
+            "phrase": "Visto que você é precioso e honrado à minha vista, e porque eu o amo, darei homens em seu lugar, e nações em troca de sua vida.",
+            "reference": "Isaías 43:4"
+        },
+        {
+            "phrase": "Nós amamos porque ele nos amou primeiro.",
+            "reference": "1 João 4:19"
+        },
+        {
+            "phrase": "O teu amor é melhor do que a vida! Por isso os meus lábios te exaltarão.",
+            "reference": "Salmos 63:3"
+        },
+        {
+            "phrase": "Estende o teu amor aos que te conhecem; a tua justiça, aos que são retos de coração.",
+            "reference": "Salmos 36:10"
+        },
+        {
+            "phrase": "O amor é paciente, o amor é bondoso. Não inveja, não se vangloria, não se orgulha.",
+            "reference": "1 Coríntios 13:4"
+        },
+        {
+            "phrase": "Amados, amemos uns aos outros, pois o amor procede de Deus. Aquele que ama é nascido de Deus e conhece a Deus. Quem não ama não conhece a Deus, porque Deus é amor.",
+            "reference": "1 João 4:7-8"
+        },
+        {
+            "phrase": "Assim conhecemos o amor que Deus tem por nós e confiamos nesse amor. Deus é amor. Todo aquele que permanece no amor permanece em Deus, e Deus nele.",
+            "reference": "1 João 4:16"
+        },
+        {
+            "phrase": "No teu templo, ó Deus, meditamos em teu amor leal.",
+            "reference": "Salmos 48:9"
+        },
+        {
+            "phrase": "O Senhor conduza o coração de vocês ao amor de Deus e à perseverança de Cristo.",
+            "reference": "2 Tessalonicenses 3:5"
+        },
+        {
+            "phrase": "A graça do Senhor Jesus Cristo, o amor de Deus e a comunhão do Espírito Santo sejam com todos vocês.",
+            "reference": "2 Coríntios 13:14"
+        }
+    ],
+    "paz": [
+        {
+            "phrase": "Lancem sobre ele toda a sua ansiedade, porque ele tem cuidado de vocês.",
+            "reference": "1 Pedro 5:7"
+        },
+        {
+            "phrase": "Portanto, não se preocupem, dizendo: 'Que vamos comer?' ou 'Que vamos beber?' ou 'Que vamos vestir?' Pois os pagãos é que correm atrás dessas coisas; mas o Pai celestial sabe que vocês precisam delas. Busquem, pois, em primeiro lugar o Reino de Deus e a sua justiça, e todas essas coisas serão acrescentadas a vocês. Portanto, não se preocupem com o amanhã, pois o amanhã trará as suas próprias preocupações. Basta a cada dia o seu próprio mal.",
+            "reference": "Mateus 6:31-34"
+        },
+        {
+            "phrase": "Não andem ansiosos por coisa alguma, mas em tudo, pela oração e súplicas, e com ação de graças, apresentem seus pedidos a Deus. E a paz de Deus, que excede todo o entendimento, guardará o coração e a mente de vocês em Cristo Jesus. Finalmente, irmãos, tudo o que for verdadeiro, tudo o que for nobre, tudo o que for correto, tudo o que for puro, tudo o que for amável, tudo o que for de boa fama, se houver algo de excelente ou digno de louvor, pensem nessas coisas.",
+            "reference": "Filipenses 4:6-8"
+        },
+        {
+            "phrase": "Mas eu, quando estiver com medo, confiarei em ti.",
+            "reference": "Salmos 56:3"
+        },
+        {
+            "phrase": "Quem de vocês, por mais que se preocupe, pode acrescentar uma hora que seja à sua vida?",
+            "reference": "Mateus 6:27"
+        },
+        {
+            "phrase": " 'Venham a mim, todos os que estão cansados e sobrecarregados, e eu darei descanso a vocês. Tomem sobre vocês o meu jugo e aprendam de mim, pois sou manso e humilde de coração, e vocês encontrarão descanso para as suas almas. Pois o meu jugo é suave e o meu fardo é leve '.",
+            "reference": "Mateus 11:28-30"
+        },
+        {
+            "phrase": "Quando a ansiedade já me dominava no íntimo, o teu consolo trouxe alívio à minha alma.",
+            "reference": "Salmos 94:19"
+        },
+        {
+            "phrase": "Levanto os meus olhos para os montes e pergunto: De onde me vem o socorro? O meu socorro vem do Senhor, que fez os céus e a terra.",
+            "reference": "Salmos 121:1-2"
+        },
+        {
+            "phrase": "Observem as aves do céu: não semeiam nem colhem nem armazenam em celeiros; contudo, o Pai celestial as alimenta. Não têm vocês muito mais valor do que elas? Quem de vocês, por mais que se preocupe, pode acrescentar uma hora que seja à sua vida?  'Por que vocês se preocupam com roupas? Vejam como crescem os lírios do campo. Eles não trabalham nem tecem.",
+            "reference": "Mateus 6:26-28"
+        },
+        {
+            "phrase": "Busquei o Senhor, e ele me respondeu; livrou-me de todos os meus temores.",
+            "reference": "Salmos 34:4"
+        },
+        {
+            "phrase": "Mesmo quando eu andar por um vale de trevas e morte, não temerei perigo algum, pois tu estás comigo; a tua vara e o teu cajado me protegem.",
+            "reference": "Salmos 23:4"
+        },
+        {
+            "phrase": "Por isso não tema, pois estou com você; não tenha medo, pois sou o seu Deus. Eu o fortalecerei e o ajudarei; eu o segurarei com a minha mão direita vitoriosa. ",
+            "reference": "Isaías 41:10"
+        },
+        {
+            "phrase": "O coração ansioso deprime o homem, mas uma palavra bondosa o anima.",
+            "reference": "Provérbios 12:25"
+        },
+        {
+            "phrase": "O coração bem disposto é remédio eficiente, mas o espírito oprimido resseca os ossos.",
+            "reference": "Provérbios 17:22"
+        },
+        {
+            "phrase": "Podemos, pois, dizer com confiança:  'O Senhor é o meu ajudador, não temerei.",
+            "reference": "O que me podem fazer os homens? ' Hebreus 13:6"
+        },
+        {
+            "phrase": "pois os nossos sofrimentos leves e momentâneos estão produzindo para nós uma glória eterna que pesa mais do que todos eles.",
+            "reference": "2 Coríntios 4:17"
+        },
+        {
+            "phrase": "Ele enxugará dos seus olhos toda lágrima. Não haverá mais morte, nem tristeza, nem choro, nem dor, pois a antiga ordem já passou '.",
+            "reference": "Apocalipse 21:4"
+        }
+    ],
+    "felicidade": [
+        {
+            "phrase": "O próprio Senhor irá à sua frente e estará com você; ele nunca o deixará, nunca o abandonará. Não tenha medo! Não desanime!",
+            "reference": "Deuteronômio 31:8"
+        },
+        {
+            "phrase": "Bem-aventurados os que choram, pois serão consolados.",
+            "reference": "Mateus 5:4"
+        },
+        {
+            "phrase": "Não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que sejam capazes de experimentar e comprovar a boa, agradável e perfeita vontade de Deus.",
+            "reference": "Romanos 12:2"
+        },
+        {
+            "phrase": "Finalmente, irmãos, tudo o que for verdadeiro, tudo o que for nobre, tudo o que for correto, tudo o que for puro, tudo o que for amável, tudo o que for de boa fama, se houver algo de excelente ou digno de louvor, pensem nessas coisas.",
+            "reference": "Filipenses 4:8"
+        },
+        {
+            "phrase": "Por que você está assim tão triste, ó minha alma? Por que está assim tão perturbada dentro de mim? Ponha a sua esperança em Deus! Pois ainda o louvarei; ele é o meu Salvador e o meu Deus.",
+            "reference": "Salmos 42:11"
+        },
+        {
+            "phrase": "Não andem ansiosos por coisa alguma, mas em tudo, pela oração e súplicas, e com ação de graças, apresentem seus pedidos a Deus. E a paz de Deus, que excede todo o entendimento, guardará o coração e a mente de vocês em Cristo Jesus.",
+            "reference": "Filipenses 4:6-7"
+        },
+        {
+            "phrase": "Lancem sobre ele toda a sua ansiedade, porque ele tem cuidado de vocês.",
+            "reference": "1 Pedro 5:7"
+        },
+        {
+            "phrase": "Entre vocês há alguém que está sofrendo? Que ele ore. Há alguém que se sente feliz? Que ele cante louvores.",
+            "reference": "Tiago 5:13"
+        },
+        {
+            "phrase": "Não deixemos de reunir-nos como igreja, segundo o costume de alguns, mas procuremos encorajar-nos uns aos outros, ainda mais quando vocês veem que se aproxima o Dia.",
+            "reference": "Hebreus 10:25"
+        },
+        {
+            "phrase": "O coração ansioso deprime o homem, mas uma palavra bondosa o anima.",
+            "reference": "Provérbios 12:25"
+        },
+        {
+            "phrase": "Os justos clamam, o Senhor os ouve e os livra de todas as suas tribulações.",
+            "reference": "Salmos 34:17"
+        },
+        {
+            "phrase": "O Espírito do Soberano, o Senhor, está sobre mim, porque o Senhor ungiu-me para levar boas notícias aos pobres. Enviou-me para cuidar dos que estão com o coração quebrantado, anunciar liberdade aos cativos e libertação das trevas aos prisioneiros, para proclamar o ano da bondade do Senhor e o dia da vingança do nosso Deus; para consolar todos os que andam tristes e dar a todos os que choram em Sião uma bela coroa em vez de cinzas, o óleo da alegria em vez de pranto e um manto de louvor em vez de espírito deprimido. Eles serão chamados carvalhos de justiça, plantio do Senhor, para manifestação da sua glória.",
+            "reference": "Isaías 61: 1-3"
+        },
+        {
+            "phrase": "Por isso não tema, pois estou com você; não tenha medo, pois sou o seu Deus. Eu o fortalecerei e o ajudarei; eu o segurarei com a minha mão direita vitoriosa.",
+            "reference": "Isaías 41: 10"
+        },
+        {
+            "phrase": "Tu és a minha lâmpada, ó Senhor! O Senhor ilumina-me as trevas.",
+            "reference": "2 Samuel 22: 29"
+        },
+        {
+            "phrase": "Depositei toda a minha esperança no Senhor; ele se inclinou para mim e ouviu o meu grito de socorro. Ele me tirou de um poço de destruição, de um atoleiro de lama; pôs os meus pés sobre uma rocha e firmou-me num local seguro. Pôs um novo cântico na minha boca, um hino de louvor ao nosso Deus. Muitos verão isso e temerão, e confiarão no Senhor.",
+            "reference": "Salmos 40:1-3"
+        },
+        {
+            "phrase": "Bendito seja o Deus e Pai de nosso Senhor Jesus Cristo, Pai das misericórdias e Deus de toda consolação, que nos consola em todas as nossas tribulações, para que, com a consolação que recebemos de Deus, possamos consolar os que estão passando por tribulações. Pois assim como os sofrimentos de Cristo transbordam sobre nós, também por meio de Cristo transborda a nossa consolação.",
+            "reference": "2 Coríntios 1:3-5"
+        },
+        {
+            "phrase": "Senhor, restaura-nos, assim como enches o leito dos ribeiros no deserto. Aqueles que semeiam com lágrimas, com cantos de alegria colherão. Aquele que sai chorando enquanto lança a semente, voltará com cantos de alegria, trazendo os seus feixes.",
+            "reference": "Salmos 126:4-6"
+        },
+        {
+            "phrase": " 'Eu disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo '.",
+            "reference": "João 16:33"
+        }
+    ],
+    "paciencia": [
+        {
+            "phrase": "Mas o fruto do Espírito é amor, alegria, paz, paciência, amabilidade, bondade, fidelidade, mansidão e domínio próprio. Contra essas coisas não há lei.",
+            "reference": "Gálatas 5:22-23"
+        },
+        {
+            "phrase": "Portanto, como povo escolhido de Deus, santo e amado, revistam-se de profunda compaixão, bondade, humildade, mansidão e paciência. Suportem-se uns aos outros e perdoem as queixas que tiverem uns contra os outros. Perdoem como o Senhor lhes perdoou. Acima de tudo, porém, revistam-se do amor, que é o elo perfeito.",
+            "reference": "Colossenses 3:12-14"
+        },
+        {
+            "phrase": "Portanto, livrem-se de toda impureza moral e da maldade que prevalece e aceitem humildemente a palavra implantada em vocês, a qual é poderosa para salvá-los.",
+            "reference": "Tiago 1:21"
+        },
+        {
+            "phrase": "Você, porém, homem de Deus, fuja de tudo isso e busque a justiça, a piedade, a fé, o amor, a perseverança e a mansidão.",
+            "reference": "1 Timóteo 6:11"
+        },
+        {
+            "phrase": "Antes, santifiquem Cristo como Senhor em seu coração. Estejam sempre preparados para responder a qualquer pessoa que pedir a razão da esperança que há em vocês. Contudo, façam isso com mansidão e respeito, conservando boa consciência, de forma que os que falam maldosamente contra o bom procedimento de vocês, porque estão em Cristo, fiquem envergonhados de suas calúnias.",
+            "reference": "1 Pedro 3:15-16"
+        },
+        {
+            "phrase": "Ao contrário, esteja no ser interior, que não perece, beleza demonstrada num espírito dócil e tranquilo, o que é de grande valor para Deus.",
+            "reference": "1 Pedro 3:4"
+        },
+        {
+            "phrase": "Irmãos, se alguém for surpreendido em algum pecado, vocês, que são espirituais, deverão restaurá-lo com mansidão. Cuide-se, porém, cada um para que também não seja tentado.",
+            "reference": "Gálatas 6:1"
+        },
+        {
+            "phrase": "O homem irado provoca brigas,\ne o de gênio violento\ncomete muitos pecados.",
+            "reference": "Provérbios 29:22"
+        },
+        {
+            "phrase": "A resposta calma desvia a fúria,\nmas a palavra ríspida desperta a ira.",
+            "reference": "Provérbios 15:1"
+        },
+        {
+            "phrase": "Ao servo do Senhor não convém brigar mas, sim, ser amável para com todos, apto para ensinar, paciente. Deve corrigir com mansidão os que se lhe opõem, na esperança de que Deus lhes conceda o arrependimento, levando-os ao conhecimento da verdade, para que assim voltem à sobriedade e escapem da armadilha do Diabo, que os aprisionou para fazerem a sua vontade.",
+            "reference": "2 Timóteo 2:24-26"
+        },
+        {
+            "phrase": "\"Mas eu digo a vocês que estão me ouvindo: Amem os seus inimigos, façam o bem aos que os odeiam, abençoem os que os amaldiçoam, orem por aqueles que os maltratam. Se alguém bater em você numa face, ofereça-lhe também a outra. Se alguém tirar de você a capa, não o impeça de tirar a túnica.",
+            "reference": "Lucas 6:27-29"
+        },
+        {
+            "phrase": "Tomem sobre vocês o meu jugo e aprendam de mim, pois sou manso e humilde de coração, e vocês encontrarão descanso para as suas almas.",
+            "reference": "Mateus 11:29"
+        }
+    ]
+  }
   var scroll = React.createRef<HTMLDivElement>();
 
 
